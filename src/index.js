57717bad4d8196362ccd04d169b276201403173a
Chris McAnally <chris@adadevelopersacademy.org>
2021-11-30 09:18:15 -0800

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 export const wordCount = (str) => {
-  return null;
+  str = str.replaceAll(/[^A-Za-z\d\'\s]/g, '');
+  const words = str.split(/\s/);
+
+  const freqMap = {};
+
+  for (let word of words) {
+    word = word.toLowerCase();
+    if (freqMap[word] === undefined) {
+      freqMap[word] = 0;
+    }
+    freqMap[word] += 1;
+  }
+
+  return freqMap;
 };
