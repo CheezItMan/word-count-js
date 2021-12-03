56562409c05c1f4ebe4b694d9d2737bdd5fd7918
Chris McAnally <chris@adadevelopersacademy.org>
2021-12-03 10:43:21 -0800

    livecode work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 export const wordCount = (str) => {
-  return null;
+  const wordCounts = {};
+
+  str = str.toLowerCase();
+  if (str === '') {
+    return wordCounts;
+  }
+
+  // Split on anything that's not:
+  //    1.  a-z
+  //    2.  0-9
+  //    3.  "'s"
+
+  str = str.replaceAll(/\'s/g, '@');
+  // "that's" -->  "that@"
+  str = str.replaceAll(/[^\w\s@]/g, '');
+  // "that@"  --> "that's"
+  str = str.replaceAll('@', "'s");
+  const words = str.split(/\s+/);
+  // 'Hello     world' ==> ['Hello', 'world]
+
+  words.forEach((word) => {
+    //word = word.toLowerCase();
+    // 'Hello' --> 'hello'
+    if (!wordCounts[word]) {
+      wordCounts[word] = 0;
+    }
+    wordCounts[word] += 1;
+  });
+  return wordCounts;
 };
--- tests/word-count.test.js
+++ tests/word-count.test.js
@@ -4,4 +4,73 @@ describe('wordCount', () => {
   test('it should exist', () => {
     expect(wordCount).toBeDefined();
   });
+
+  test('counts one word', () => {
+    //arrange
+    const word = 'hello';
+
+    //act/assert
+    expect(wordCount(word)).toEqual({ hello: 1 });
+  });
+
+  test('returns an empty object for empty string', function () {
+    // Arrange
+    const str = '';
+
+    // Act
+    const result = wordCount(str);
+
+    // Assert
+    expect(result).toEqual({});
+  });
+
+  test('counts two lowercase words', () => {
+    // Arrange
+    const str = 'hello world';
+
+    // Act
+    const result = wordCount(str);
+
+    // Assert
+    expect(result).toEqual({
+      hello: 1,
+      world: 1,
+    });
+  });
+
+  test('ignore case', () => {
+    // Arrange
+    const str = 'hello world Hello WORLD';
+
+    // Act
+    const result = wordCount(str);
+
+    // Assert
+    expect(result).toEqual({
+      hello: 2,
+      world: 2,
+    });
+  });
+
+  test(`ignores special characters except 's`, () => {
+    // Arrange
+    const str = `"That's the password: 'PASSWORD 123'!", cried the Special Agent.\nSo I fled.`;
+
+    // Act
+    const result = wordCount(str);
+
+    // Arrange
+    expect(result).toEqual({
+      "that's": 1,
+      the: 2,
+      password: 2,
+      123: 1,
+      cried: 1,
+      special: 1,
+      agent: 1,
+      so: 1,
+      i: 1,
+      fled: 1,
+    });
+  });
 });
